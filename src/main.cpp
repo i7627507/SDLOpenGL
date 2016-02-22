1ed78d59283e1d8bc8eb3e7df2ebb468e8568e03
i7627507 <i7627507@bournemouth.ac.uk>
2016-02-22 11:55:46 +0000

    initiale and more

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "SDLOpenGL.h"
+#include <ngl/NGLInit.h>
+
+void initializeGL();
+void renderGL();
+
+int main(int argc, char *argv[])
+{
+
+  SDLOpenGL window("test",0,0,1024,720); //Constructor of the window class in SDLOpenGL.h
+  window.makeCurrent();
+  initializeGL();
+  while(true)
+  {
+         renderGL();
+         window.swapWindow();
+         SDL_Delay(10);
+       }
+  std::cout<<"Working\n";
+}
+
+void initializeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.8,0.8,0.8,1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
